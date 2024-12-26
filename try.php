4a0ddc36ac345973f3c4024ac82e49fa97d7b02c
Kiran dhoke <dhokekiran98@gmail.com>
2024-12-26 12:55:38 +0530

    trying

--- try.php
+++ try.php
@@ -1,19 +1,31 @@
 <?php
-function searchAdditionalServices($jsonResponse) {
-    // Decode the JSON response
-    $responseArray = json_decode($jsonResponse, true);
+$curl = curl_init();
 
-    // Convert the array to a JSON string
-    $jsonString = json_encode($responseArray);
+curl_setopt_array($curl, array(
+  CURLOPT_URL => 'http://xmlapiv3.ypsilon.net:10816',
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'POST',
+  CURLOPT_POSTFIELDS =>'<?xml version=\'1.0\' encoding=\'UTF-8\'?><fareRequest xmlns:shared="http://ypsilon.net/shared" da="true"><vcrs><vcr>QF</vcr></vcrs><alliances/><shared:fareTypes/><tourOps/><flights><flight depDate="2025-02-12" dstApt="DEL" depApt="MEL"/></flights><paxes><pax gender="M" surname="Klenz" firstname="Hans A ADT" dob="1945-12-12"/></paxes><paxTypes/><options><limit>20</limit><offset>0</offset><vcrSummary>false</vcrSummary><waitOnList><waitOn>ALL</waitOn></waitOnList></options><coses/></fareRequest>',
+  CURLOPT_HTTPHEADER => array(
+    'accept: application/xml',
+    'accept-encoding: gzip',
+    'api-version: 3.92',
+    'accessmode: agency',
+    'accessid: gaura gaura',
+    'authmode: pwd',
+    'authorization: Basic c2hlbGx0ZWNoOjRlNDllOTAxMGZhYzA1NzEzN2VjOWQ0NWZjNTFmNDdh',
+    'content-Length: 494',
+    'Connection: close',
+    'Content-Type: text/plain'
+  ),
+));
+$response = curl_exec($curl);
 
-    // Search for the word "additionalServices" in the JSON string
-    if (strpos($jsonString, 'additionalServices') !== false) {
-        return true;
-    } else {
-        return false;
-    }
-}
-// Call the function and print the result
-$services = searchAdditionalServices($jsonResponse);
-var_dump($services);  // Outputs: bool(true) or bool(false)
+curl_close($curl);
+echo $response;
 ?>
